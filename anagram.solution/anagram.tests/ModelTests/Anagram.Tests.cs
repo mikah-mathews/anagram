14606edd6ab123c0517ad3aa9f1f09e63af0def4
mai cook <mailiecook@yahoo.com>
2020-05-12 13:24:06 -0700

    initiated project repo

---
+++ anagram.solution/anagram.tests/ModelTests/Anagram.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Anagram.Model;
+
+namespace Anagram.Tests
+{
+  [TestClass]
+  public class PhraseTests
+  {
+    
+  }
+}
